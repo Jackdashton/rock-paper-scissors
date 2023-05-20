ea23b77340ff83cd6a8efa644d1b5c47e6062277
jack ashton <jackdashton@outlook.com>
2023-05-20 18:51:39 +0100

    finished js, bugged

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ function game() {
     playerOptions.forEach((option) => {
       option.addEventListener("click", function () {
         const movesLeft = document.querySelector(".movesleft");
-        moves += 1;
-        movesLeft.innerText = `Moves left: ${10 - moves}`;
+        moves++;
+        movesLeft.innerText = `Moves Left: ${10 - moves}`;
 
         const choiceNumber = Math.floor(Math.random() * 3);
         const computerChoice = computerOptions[choiceNumber];
@@ -32,49 +32,80 @@ function game() {
       });
     });
   }
+
+  // Decide winner
+  function winner(computer, player) {
+    const result = document.querySelector(".result");
+    const playerScoreBoard = document.querySelector(".p-count");
+    const computerScoreBoard = document.querySelector(".c-count");
+    player = player.toLowerCase();
+    computer = computer.toLowerCase();
+
+    if (computer === player) {
+      result.textContent = "Tie";
+    } else if (player === "rock") {
+      if (computer === "paper") {
+        result.textContent = "Computer Won";
+        computerScore = +1;
+        computerScoreBoard.textContent = computerScore;
+      } else {
+        result.textContent = "Player Won";
+        playerScore = +1;
+        playerScoreBoard.textContent = playerScore;
+      }
+    } else if (player === "scissors") {
+      if (computer === "rock") {
+        result.textContent = "Computer Won";
+        computerScore = +1;
+        computerScoreBoard.textContent = computerScore;
+      } else {
+        result.textContent = "Player Won";
+        playerScore = +1;
+        playerScoreBoard.textContent = playerScore;
+      }
+    } else if (player === "paper") {
+      if (computer === "scissors") {
+        result.textContent = "Computer Won";
+        computerScore = +1;
+        computerScoreBoard.textContent = computerScore;
+      } else {
+        result.textContent = "Player Won";
+        playerScore = +1;
+        playerScoreBoard.textContent = playerScore;
+      }
+    }
+  }
+
+  // Run function when the game finishes
+  const gameOver = (playerOptions, movesLeft) => {
+    const chooseMove = document.querySelector(".move");
+    const result = document.querySelector(".result");
+    const reloadBtn = document.querySelector(".reload");
+
+    playerOptions.forEach((option) => {
+      option.style.display = "none";
+    });
+
+    chooseMove.innerText = "Game over!!";
+    movesLeft.style.display = "none";
+
+    if (playerScore > computerScore) {
+      result.innerText = "You won the game!";
+    } else if (playerScore < computerScore) {
+      result.innerText = "You lost the game!";
+    } else {
+      result.innerText = "Tie!";
+    }
+    reloadBtn.innerText = 'Restart';
+    reloadBtn.style.display = 'flex'
+    reloadBtn.addEventListener('click',() => {
+      window.location.reload();
+    })
+  };
+
+  // play game function
+  playGame();
 }
 
-// Decide winner
-function winner(computer, player) {
-  const result = document.querySelector('.result');
-  const playerScoreBoard = document.querySelector('.p-count');
-  const computerScoreBoard = document.querySelector('.c-count');
-  player = player.toLowerCase();
-  computer = computer.toLowerCase();
-
-  if (computer === player){
-    result.textContent = "Tie"
-  } else if (player === "rock"){
-    if(computer === "paper") {
-      result.textContent = "Computer Won";
-      computerScore =+ 1 ;
-      computerScoreBoard.textContent = computerScore;
-    } else{
-      result.textContent = "Player Won";
-      playerScore =+ 1 ;
-      playerScoreBoard.textContent = playerScore;
-    }
-  }
-  else if (player === "scissors"){
-    if(computer === "rock") {
-      result.textContent = "Computer Won";
-      computerScore =+ 1 ;
-      computerScoreBoard.textContent = computerScore;
-    } else{
-      result.textContent = "Player Won";
-      playerScore =+ 1 ;
-      playerScoreBoard.textContent = playerScore;
-    }
-  }
-  else if (player === "paper"){
-    if(computer === "scissors") {
-      result.textContent = "Computer Won";
-      computerScore =+ 1 ;
-      computerScoreBoard.textContent = computerScore;
-    } else{
-      result.textContent = "Player Won";
-      playerScore =+ 1 ;
-      playerScoreBoard.textContent = playerScore;
-    }
-  }
-};
+// game function
+game();
